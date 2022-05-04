56f650384a751e0200b23727d75cccdeda2699b0
Roberto Montalti <roberto.montalti00@gmail.com>
2022-05-04 20:34:55 +0200

    Check if quom is detected by the current bash, use python if it's still undetected after installation

--- quom.sh
+++ quom.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-if !(hash quom &>/dev/null;) then
+QUOM_COMMAND="quom"
+
+if !(hash $QUOM_COMMAND &>/dev/null;) then
     pip install --user quom
 fi
 
-quom src/json_parser.cpp header_only/jp.hpp \
+# quom path might not be registered in PATH env (It could happen when using mingw)
+if !(hash $QUOM_COMMAND &>/dev/null;) then
+    QUOM_COMMAND="python -m quom"
+fi
+
+$QUOM_COMMAND src/json_parser.cpp header_only/jp.hpp \
     --include_directory include \
     --source_directory src
 
